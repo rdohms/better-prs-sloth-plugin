5899c97856331f57afef6e5d6e0e8726c905826c
Rafael Dohms <rdohms@gmail.com>
2016-05-21 22:34:53 +0200

    Updates
    - added README
    - renamed composer package name
    - added examples

--- config/plugin.php
+++ config/plugin.php
@@ -1,14 +1,21 @@
 <?php
 
+use DMS\Sloth\Plugin\Github\LabelManager\Action\NotifyLabelAction;
 use DMS\Sloth\Plugin\Github\PullRequestManager\Action\BroadcastPrComment;
 use DMS\Sloth\Plugin\Github\PullRequestManager\Action\BroadcastPrReviewComment;
 use Sloth\Platform\Web\Factory\GenericSlackAwareFactory;
 
 return [
+
+    'dms.github.label-manager' => [
+        'actionable.regexp' => '/^needs.*$/'
+    ],
+
     'dependencies' => [
         'factories' => [
             BroadcastPrComment::class       => GenericSlackAwareFactory::class,
-            BroadcastPrReviewComment::class => GenericSlackAwareFactory::class
+            BroadcastPrReviewComment::class => GenericSlackAwareFactory::class,
+            NotifyLabelAction::class        => GenericSlackAwareFactory::class
         ],
     ],
 
@@ -23,6 +30,12 @@ return [
             'name' => 'plugin_dms_pr_regular_review_comment',
             'path' => '/plugin/dms/pr/review-comment',
             'middleware' => BroadcastPrReviewComment::class,
+            'allowed_methods' => ['POST'],
+        ],
+        [
+            'name' => 'plugin_dms_label_notify_label',
+            'path' => '/plugin/dms/label/notify',
+            'middleware' => NotifyLabelAction::class,
             'allowed_methods' => ['POST'],
         ],
     ],
